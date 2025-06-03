8719249b2d773bdcaf1cb5f132117b5017cf881a
Chamosio <chamosio.kebbaboni@gmail.com>
2025-06-03 13:54:39 +0300

    Happy pride month!

--- src/main.rs
+++ src/main.rs
@@ -161,6 +161,7 @@ fn find_empty_temp_filename() -> String {
 }
 
 fn main() {
+    println!("Happy pride month! 🏳️‍🌈");
     let args: Vec<String> = std::env::args().collect();
     if args.len() == 1 {
         print!("{}", HELP);
